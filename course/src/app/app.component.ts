430ad77d4767451250f59f7af0c4fb72bc4f4216
Rafael Ortiz <rafael@helloinsight.org>
2024-03-13 14:48:23 -0500

    Interpolation

--- course/src/app/app.component.ts
+++ course/src/app/app.component.ts
@@ -6,8 +6,12 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'course';
+
+  onKeyUp(title: string) {
+    this.title = title;
+  }
 }
